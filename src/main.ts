186665ee32da94ef85ad53013084460d9165180e
adriandcoding <theamr91@gmail.com>
2024-05-04 09:20:41 +0200

    cambios

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,9 @@
-let puntos = 0;
+let puntos = 8;
 const mostrarPuntuacion = () => {
   const Puntuación = document.querySelector(".puntuacion");
   Puntuación.innerHTML = puntos;
 };
 document.addEventListener("DOMContentLoaded", mostrarPuntuacion());
 
-const BotonDameCarta = document.querySelector(".pedir-carta");
-const dameCarta = () => {
-    puntos = 0
-    const carta = Math.floor(Math.random() * 100) + 1;
-    puntos += carta;
-    return puntos
-}
-BotonDameCarta.addEventListener("click", dameCarta);
+
 
