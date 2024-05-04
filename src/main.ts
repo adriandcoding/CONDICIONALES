de3afd528216c96dfb66cf5e387b0d239504c1d9
adriandcoding <theamr91@gmail.com>
2024-05-04 10:56:23 +0200

    cambios

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,12 @@ const mostrarPuntuacion = () => {
   const Puntuación = document.querySelector(".puntuacion");
   Puntuación.innerHTML = puntos;
 };
-document.addEventListener("DOMContentLoaded", mostrarPuntuacion());
+document.addEventListener("DOMContentLoaded", mostrarPuntuacion);
 
+const generarNumeroAleatorio = (min: number, max: number): number => {
+  let numeroAleatorio = Math.floor(Math.random() * (max - min + 1) + min);
+  return numeroAleatorio;
+}
+  
 
-
+console.log(generarNumeroAleatorio(1,12));
